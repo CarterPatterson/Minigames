24263856300c8758efd23ab51ff76648e14f97cb
Carter <carter@gamemodeone.com>
2022-11-11 18:19:14 -0800

    Update scene

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -5,21 +5,40 @@ namespace Player
 {
     public class CharacterMovement : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        private Rigidbody rigidbody;
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        public float movementSpeed;
 
         private void Awake()
         {
+            rigidbody = gameObject.GetComponent(typeof(Rigidbody)) as Rigidbody;
+        }
+
+        private void Update()
+        {
+            var velocity = new Vector3(0, 0, 0);
             
+            if (Input.GetKey(KeyCode.W))
+            {
+                velocity.z += 1;
+            }
+            
+            if (Input.GetKey(KeyCode.S))
+            {
+                velocity.z -= 1;
+            }
+            
+            if (Input.GetKey(KeyCode.A))
+            {
+                velocity.x -= 1;
+            }
+            
+            if (Input.GetKey(KeyCode.D))
+            {
+                velocity.x += 1;
+            }
+
+            rigidbody.AddForce(velocity * movementSpeed);
         }
     }
 }
